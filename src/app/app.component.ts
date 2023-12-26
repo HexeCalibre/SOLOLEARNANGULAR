4f41d3c8512e62481ed2d0ea7ad20ec3fffc1699
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-12-26 16:05:32 +0800

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { NotesListComponent } from '../notes-list/notes-list.component';
+import { AddNoteComponent } from '../add-note/add-note.component';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+/* import { FormsModule } from '@angular/forms';
+How to use:
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ FormsModule ],
+  export class AppComponent {
+  name = '';
+  showName() {
+    alert(this.name);
+  }
+}
+<form (ngSubmit)="showName()" #myForm="ngForm">
+  <label> Name: </label>
+  <input type="text" [(ngModel)]="name" name="name" required/>
+  <input type="submit" value="Submit" [disabled]="!myForm.form.valid"/>
+</form>
+*/
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    NotesListComponent,
+    AddNoteComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'SololearnAngular';
+
+  // FORM CONTROL
+  // name = new FormControl('');
+  // showName() {
+  //   alert(this.name.value);
+  // }
+
+  // FORMGROUP
+  // loginForm = new FormGroup({
+  //   username: new FormControl('',Validators.required),
+  //   password: new FormControl('',Validators.required),
+  // });
+  // login() {
+  //   alert(
+  //     this.loginForm.value.username +
+  //     ' | ' + 
+  //     this.loginForm.value.password
+  //   );
+  // }
+
 }
